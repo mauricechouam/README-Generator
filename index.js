dab73948d382424da9918a0524f9bd4d51bdf5da
mauricechouam <lansichouamou@gmail.com>
2020-06-01 20:51:45 -0700

    added Function to generate readme.md file

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function prompUser() {
             name: "installation",
             message: "Give few step how to install your application "
         },
-       
+
         {
             type: "input",
             name: "usage",
@@ -75,42 +75,38 @@ function generateReadme(answer) {
   # ${answer.Projecttitle}
 
   ## Table of Contents ##
-  * [Project_descriptiion](#Project_descriptiion)
+  * [Project_description](#Project_description)
   * [Installation](#Installation)
   * [User_Story](#User_Story)
   * [Usage]#(Usage)
   * [Licence](#Wireframe)
   * [Tasks](#Tasks)
-  * [Roles](#Roles)
-  
-  ## User Story ##
-  
-  AS A concerned citizen of the world, looking out for myself and my loved ones.
-  I WANT to get up to date information on the spread of COVID-19 in my area.
-  SO THAT I can make safe plans for shopping, working and living in my community.
+  * [Contributing](#Contributing)
+  * [Question](#Question)
   
   
-  ## Roles ##
-  
-  * Chase - Map & Global Stats API, Frontend & CSS styling.
-  * David - News, Testing & additional Stats API, plus some CSS.
-  * Maurice - Map API, Frontend & CSS styling. 
-  
- 
-  ## Future-Development ##
-  
-  * Update Map API to display more information.
-  * Add diagrams to sync with statistics.
-  * Developement desktop application.
-  
-  ## Sources ##
-  
-  - Moment.JS :  Parse, validate, manipulate, and display dates and times
-  - Popper.js :  Popover positioning engine, in conjunction with Bootstrap.js
-  - JQueryUI.js : Interactions and animations, in conjunction with jQuery.js
-  - The Nerdy Dev : You Tuber Whose Video helped us create our MapBox panel
-  
+
+## Project description
+${answer.Description}
+
+
+ ## User Story ## 
+
+ ${answer.userstory}
+
+
   
   `;
-  }
-  
+}
+
+prompUser()
+    .then(function (answer) {
+    const md = generateReadme(answer);
+    return writeFileAsync("README.md", md)
+    })
+    .then(function () {
+        console.log("successfully  generate README.md");
+    })
+    .catch(function (err) {
+        console.log(err);
+    })
