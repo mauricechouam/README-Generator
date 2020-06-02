e180a0f7418c4aea1133e24b25cda4e790367414
mauricechouam <lansichouamou@gmail.com>
2020-06-01 20:35:54 -0700

    added  function generateReadme()

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ function prompUser() {
             name: "Description",
             message: "Enter the Project Description"
         },
+        {
+            type: "input",
+            name: "userstory",
+            message: "Enter the User Story"
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Give few step how to install your application "
+        },
        
         {
             type: "input",
@@ -59,11 +69,48 @@ function prompUser() {
     ])
 }
 
+// Function generateReadme
 function generateReadme(answer) {
     return `
-  # ${answer.title}
+  # ${answer.Projecttitle}
+
+  ## Table of Contents ##
+  * [Project_descriptiion](#Project_descriptiion)
+  * [Installation](#Installation)
+  * [User_Story](#User_Story)
+  * [Usage]#(Usage)
+  * [Licence](#Wireframe)
+  * [Tasks](#Tasks)
+  * [Roles](#Roles)
+  
+  ## User Story ##
+  
+  AS A concerned citizen of the world, looking out for myself and my loved ones.
+  I WANT to get up to date information on the spread of COVID-19 in my area.
+  SO THAT I can make safe plans for shopping, working and living in my community.
+  
+  
+  ## Roles ##
+  
+  * Chase - Map & Global Stats API, Frontend & CSS styling.
+  * David - News, Testing & additional Stats API, plus some CSS.
+  * Maurice - Map API, Frontend & CSS styling. 
+  
+ 
+  ## Future-Development ##
+  
+  * Update Map API to display more information.
+  * Add diagrams to sync with statistics.
+  * Developement desktop application.
+  
+  ## Sources ##
+  
+  - Moment.JS :  Parse, validate, manipulate, and display dates and times
+  - Popper.js :  Popover positioning engine, in conjunction with Bootstrap.js
+  - JQueryUI.js : Interactions and animations, in conjunction with jQuery.js
+  - The Nerdy Dev : You Tuber Whose Video helped us create our MapBox panel
+  
   
   `;
   }
   
-  module.exports = generateMarkdown;
